be49ed2f8d1960e7853f7b1e12a6824e03c98250
Gornostaev Sergey <sergey@gornostaev.pro>
2015-07-16 23:12:59 +0300

    Added Play button and keyboard events

--- src/state/game.js
+++ src/state/game.js
@@ -2,7 +2,32 @@ var game = function(izojump) {
 };
 
 game.prototype = {
+    init: function(info) {
+        this.keyboard = true;
+    },
     create: function() {
         this.stage.backgroundColor = '#fbfaf7';
+
+        this.input.keyboard.addCallbacks(this, null, function() {
+            this.keyboard = true;
+        });
+    },
+    update: function() {
+        if (this.keyboard && this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            this.keyboard = false;
+            console.log('Left');
+        }
+        if (this.keyboard && this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            this.keyboard = false;
+            console.log('Right');
+        }
+        if (this.keyboard && this.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            this.keyboard = false;
+            console.log('Up');
+        }
+        if (this.keyboard && this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            this.keyboard = false;
+            console.log('Down');
+        }
     }
 };
--- src/state/menu.js
+++ src/state/menu.js
@@ -5,7 +5,12 @@ menu.prototype = {
     create: function() {
         this.stage.backgroundColor = '#fbfaf7';
 
-        this.input.onDown.addOnce(function() {
+        var play_button = this.add.text(this.world.centerX, this.world.centerY, "Play", {font: "28px Arial", fill: "#222"});
+        play_button.anchor.set(0.5);
+        play_button.inputEnabled = true;
+        play_button.input.useHandCursor = true;
+
+        play_button.events.onInputDown.add(function() {
             this.game.state.start("game");
         }, this);
     }
